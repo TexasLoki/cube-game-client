1de0b960724b4f98977bcd51fb2d78164d03e833
tobbebia <tobbe.bia@gmail.com>
2012-07-14 21:01:35 +0200

    V toggles wireframe render mode

--- src/game/Game.java
+++ src/game/Game.java
@@ -33,6 +33,7 @@ public class Game {
 	private boolean flyMode = true;
 	private boolean doCollisionChecking = false;
 	private boolean renderSkybox = true;
+	private boolean wireframe = false;
 	
 	// Moving cube (trollface)
 	private Cube movingCube;
@@ -138,6 +139,11 @@ public class Game {
 			GL11.glClearColor(0.25f, 0.8f, 1.0f, 1.0f);
 			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 			GL11.glLoadIdentity();
+			
+			if(wireframe)
+				GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
+			else 
+				GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
 
 			// Apply the camera matrix
 			camera.applyMatrix();
@@ -157,7 +163,7 @@ public class Game {
 			GL11.glColorMaterial(GL11.GL_FRONT_AND_BACK, GL11.GL_AMBIENT_AND_DIFFUSE);
 			GL11.glEnable(GL11.GL_COLOR_MATERIAL);
 			
-			GL11.glColor3f(1.0f, 0.0f, 0.0f);
+			GL11.glColor3f(0.6f, 0.2f, 0.3f);
 
 			objModelTest.render(new Vector3f(0.0f, -30.0f, 0.0f), new Vector3f(0.0f, 45.0f, 0.0f), new Vector3f(1.0f, 1.0f, 1.0f));
 			
@@ -211,6 +217,8 @@ public class Game {
 					    	doCollisionChecking = !doCollisionChecking;
 					} else if (Keyboard.getEventKey() == Keyboard.KEY_B) {
 					    	renderSkybox = !renderSkybox;
+					} else if (Keyboard.getEventKey() == Keyboard.KEY_V) {
+					    	wireframe = !wireframe;
 					}
 				}
 			}
