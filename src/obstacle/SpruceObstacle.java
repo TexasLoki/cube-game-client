ae18f7a55a883d41f47fe2d3d802eeb18bfce798
tobbebia <tobbe.bia@gmail.com>
2012-11-12 15:48:46 +0100

    Made some changes to the spruce tree obstacle

--- src/obstacle/SpruceObstacle.java
+++ src/obstacle/SpruceObstacle.java
@@ -12,34 +12,49 @@ public class SpruceObstacle extends Obstacle {
 	}
 	
 	public void createSpruce(boolean textures) {
-		// Specify size
-		xLength = 7;
-		zLength = 7;
-		yLength = 8;
 		
-		// Create array
+		// Defines how the spruce tree will look
+		int stemHeight = 17;
+		int stemThickness = 1;
+		int crownStartY = 5;	// Counting from 0
+		int crownHeightPerLevel = 3;
+		
+		// Calculate the obstacle array size
+		xLength = (int) Math.ceil((float)(stemHeight - crownStartY) / (float)crownHeightPerLevel) * 2 + stemThickness;
+		zLength = (int) Math.ceil((float)(stemHeight - crownStartY) / (float)crownHeightPerLevel) * 2 + stemThickness;
+		yLength = stemHeight + crownHeightPerLevel;
+		
+		// Create the array
 		obstacleArray = new Cube[xLength][yLength][zLength];
 		
-		for(int x = 0; x < xLength; x++) {
-			for(int y = 0; y < yLength; y++) {
-				for(int z = 0; z < zLength; z++) {
-					obstacleArray[x][y][z] = null;
+		// Create the green crown/leaves
+		for(int y = stemHeight; y < stemHeight + crownHeightPerLevel; y++) {
+			for(int x = 0; x < stemThickness; x++) {
+				for(int z = 0; z < stemThickness; z++) {
+					obstacleArray[(xLength - stemThickness) / 2 + x][y][(zLength - stemThickness) / 2 + z] = new Cube(null, null, new Vector4f(0.0f, 0.20f, 0.04f, 1.0f), null);
 				}
 			}
 		}
 		
-		// Create spruce "crown"
-		for(int y = 3; y < yLength; y++) {
-			for(int x = y - 3; x < xLength - (y - 3); x++) {
-				for(int z = y - 3; z < zLength - (y - 3); z++) {
-					obstacleArray[x][y][z] = new Cube(null, null, new Vector4f(0.0f, 0.20f, 0.04f, 1.0f), null);
+		for(int y = stemHeight - 1; y >= crownStartY; y--) {
+			int rectSize = (int) Math.ceil((float)(stemHeight - y) / (float)crownHeightPerLevel) * 2 + stemThickness;
+			
+			for(int x = 0; x < rectSize; x++) {
+				for(int z = 0; z < rectSize; z++) {
+					obstacleArray[(xLength - rectSize) / 2 + x][y][(zLength - rectSize) / 2 + z] = new Cube(null, null, new Vector4f(0.0f, 0.20f, 0.04f, 1.0f), null);
 				}
 			}
+			
 		}
 		
-		// Create stem
-		for(int y = 0; y < yLength - 2; y++) {
-			obstacleArray[xLength/2][y][zLength/2] = new Cube(null, null, new Vector4f(0.25f, 0.125f, 0.0f, 1.0f), null);
+		// Create the stem
+		for(int y = 0; y < stemHeight; y++) {
+			for(int x = 0; x < stemThickness; x++) {
+				for(int z = 0; z < stemThickness; z++) {
+					obstacleArray[(xLength - stemThickness) / 2 + x][y][(zLength - stemThickness) / 2 + z] = new Cube(null, null, new Vector4f(0.25f, 0.125f, 0.0f, 1.0f), null);
+				}
+			}
+			
 		}
 	}
 	
